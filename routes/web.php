c900fdda8a33f256a32c5991cf200ae1c9cab514
DuckWithBoots <stubenvoll.emanuel@gmail.com>
2025-01-23 13:49:11 +0000

    added smiley template

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WelcomeController;
+use App\Http\Controllers\{WelcomeController, SurveyController};
+use Livewire\Volt\Volt;
 
 Route::get('/', [WelcomeController::class, 'render']);
 Route::post('/', [WelcomeController::class, 'validate']);
+
+Route::get('feedback/smiley', [SurveyController::class, 'showSmiley']);
+
+
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
