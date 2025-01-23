dd871181d916e2cb0153e3a3e65b4166b6dfb7c0
DuckWithBoots <stubenvoll.emanuel@gmail.com>
2025-01-23 07:20:39 +0000

    added Seeder Files

---
+++ database/seeders/FeedbackTemplateSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class FeedbackTemplateSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
