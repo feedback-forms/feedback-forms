273a6bcbf1a2dc5d837c9390d9f18dd3c353e54e
Jan Langguth <jl@delife.de>
2025-02-27 13:56:26 +0000

    Ezatla Fix: Filterlogik JS ausgelagert -Fix #66

--- resources/views/livewire/surveys/overview.blade.php
+++ resources/views/livewire/surveys/overview.blade.php
@@ -12,56 +12,6 @@
             <button class="filter-button" id="surveys-filter-cancelled" filter-type="cancelled">{{__('surveys.cancelled')}}</button>
         </div>
 
-        <script>
-            const surveyButtons = document.querySelectorAll('[id^="surveys-filter-"]');
-
-            function watchSurveys() {
-                surveyButtons.forEach(button => {
-                    button.addEventListener('click', () => {
-                        button.classList.toggle('active');
-                        filterSurveys();
-                    });
-                });
-            }
-
-            function filterSurveys() {
-                let activeFilters = [];
-                let surveys = document.querySelectorAll('.survey-wrapper');
-
-                surveyButtons.forEach(button => {
-                    if (button.classList.contains('active')) {
-                        activeFilters.push(button.getAttribute('filter-type'));
-                    }
-                });
-
-                surveys.forEach(survey => survey.classList.add('hidden'));
-
-                if (activeFilters.length > 0) {
-                    activeFilters.forEach(activeFilter => displaySurveys(activeFilter));
-                }
-                else {
-                    displaySurveys(null);
-                }
-            }
-
-
-            function displaySurveys(activeFilter) {
-                let surveys = document.querySelectorAll('.survey-wrapper');
-
-                surveys.forEach(survey => {
-                    if (activeFilter == null) {
-                        survey.classList.remove('hidden');
-                    }
-
-                    if (survey.getAttribute('filter-type') === activeFilter) {
-                        survey.classList.remove('hidden');
-                    }
-                });
-            }
-
-            watchSurveys();
-        </script>
-
         <div class="flex flex-row gap-10 flex-wrap justify-center">
             @foreach($items as $item)
                 <!-- filter-type="running" ist hier nur als Beispiel drinnen. "running" dann zu dem tatsächlichen Status der survey ändern. (expired, running, cancelled) -->
