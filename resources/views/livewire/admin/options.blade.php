b7f76fa14c9b2866027e4ce5fa928383fa1c6508
DuckWithBoots <stubenvoll.emanuel@gmail.com>
2025-03-13 14:16:40 +0000

    forgor modal button design :(

--- resources/views/livewire/admin/options.blade.php
+++ resources/views/livewire/admin/options.blade.php
@@ -223,9 +223,10 @@
                     <x-secondary-button type="button" x-on:click="$dispatch('close-modal', 'edit-option')">
                         {{ __('options.modal.cancel') }}
                     </x-secondary-button>
-                    <x-primary-button type="submit">
+                    <button type="submit" 
+                            class="inline-flex items-center px-4 py-2 bg-indigo-600 dark:bg-indigo-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 dark:hover:bg-indigo-600 focus:bg-indigo-700 dark:focus:bg-indigo-600 active:bg-indigo-900 dark:active:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                         {{ __('options.modal.button') }}
-                    </x-primary-button>
+                    </button>
                 </div>               
             </form>
         </div>
