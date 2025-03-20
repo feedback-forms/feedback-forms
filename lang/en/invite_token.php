cee516db55cb4d31bb266616cccad1fa28d77883
Johannes Thiem <johannesthiem6@gmail.com>
2025-03-20 13:59:08 +0000

    Add language files for authentication, navigation, notifications, pagination, passwords, profile, registration, validation, and welcome messages. Update existing language files with new keys for better user experience and privacy considerations.

---
+++ lang/en/invite_token.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'add_token' => 'Create New Invitation Token',
+    'token' => 'Token',
+    'duration' => 'Duration in days',
+    'created_time_ago' => 'created :time ago',
+    'expired_time_ago' => 'expired :time ago',
+    'expires_in' => 'expires in :time',
+    'cancel' => 'Cancel',
+    'create' => 'Create',
+    'revoke' => 'Revoke',
+    'change_duration' => 'Change Duration',
+    'edit' => 'Edit',
+];
