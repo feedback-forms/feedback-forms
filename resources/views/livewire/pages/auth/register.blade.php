bcce9909d47fd5e423cab90730e3fd846cdfebb0
Armin Burger <56692081+techmaved@users.noreply.github.com>
2025-03-21 11:07:30 +0100

    remove wire:navigate spa navigation

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -103,7 +103,7 @@ new #[Layout('layouts.guest')] class extends Component {
 
         <div class="flex items-center justify-between mt-4">
             <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
-               href="{{ route('login') }}" title="Zum Login" wire:navigate>
+               href="{{ route('login') }}" title="Zum Login">
                 {{ __('register.already-registered') }}
             </a>
 
