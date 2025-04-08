a485f4d03f79850a4b8e12901dab1a75d83cad33
Armin Burger <56692081+techmaved@users.noreply.github.com>
2025-04-08 14:06:48 +0200

    fix naming of product

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Feedback-Forms'),
+    'name' => env('APP_NAME', 'Feedback Forms'),
 
     /*
     |--------------------------------------------------------------------------
